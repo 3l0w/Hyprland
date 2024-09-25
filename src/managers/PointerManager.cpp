f5424696cf8ef1bc41544bd27387c70d05fdbb51
Gwilherm Folliot <gwilherm55fo@gmail.com>
2024-09-25 18:03:12 +0200

    input-capture: impl keyboard, mouse button & mouse wheel

--- src/managers/PointerManager.cpp
+++ src/managers/PointerManager.cpp
@@ -694,6 +694,7 @@ void CPointerManager::move(const Vector2D& deltaLogical) {
 
     PROTO::inputCapture->sendAbsoluteMotion(newPos, deltaLogical);
 
+    //TODO: Inhibit inputs
     warpTo(newPos);
 }
 
